59f6a6adb19a2fb0b976c5d392fce71614885972
gopher <gopher.hu@gmail.com>
2023-03-21 16:50:57 +0100

    Improve the example

--- example/index.php
+++ example/index.php
@@ -12,33 +12,40 @@ class MyController {
 
     private $view;
 
-    public function __construct(View $view) {
+    public function __construct(View $view) { // the View parameter will be automatically injected
         $this->view = $view;
     }
     
     public function index() {
+        // render index.phtml with layout.phtml
         return $this->view->layout('index');
     }
 
     public function example($parameter) {
+        // the path variable value will be in '$parameter'
         return $this->view->layout('index', [
-            'parameter' => $parameter
+            'parameter' => $parameter // give the parameter for the view
         ]); 
     }
 }
 
-class MyApp extends WebApp {
+class MyApp extends WebApp { // inherit from WebApp for an MVC web application
 
     public function __construct(array $configPaths) {
         parent::__construct($configPaths);
+
+        // register the MyController class for dependency injection
         $this->add(MyController::class);
     }
 
     public function init() {
         parent::init();
-        $myController = $this->get(MyController::class);
-        $this->router->add('/', [$myController, 'index']);
-        $this->router->add('/example/?', [$myController, 'example']);
+
+        // add endpoint for home
+        $this->router->add('/', [MyController::class, 'index']);
+
+        // add endpoint with path variable
+        $this->router->add('/example/?', [MyController::class, 'example']);
     }    
 }
 
