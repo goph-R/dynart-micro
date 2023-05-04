231bfac46c31ce53d70da7df378b8a0daf31b5fa
gopher <gopher.hu@gmail.com>
2023-05-04 08:43:13 +0200

    Refactor Repository

--- src/Database/Repository.php
+++ src/Database/Repository.php
@@ -10,17 +10,26 @@ abstract class Repository {
 
     /** @var Database */
     protected $db;
-    protected $table;
+    protected $tableName;
     protected $sqlParams = [];
     protected $orderByFields = [];
-    protected $allFields = [];
 
     public function __construct(Database $db) {
         $this->db = $db;
     }
 
+    abstract public function allFields();
+
+    public function tableName() {
+        return $this->db->configValue('table_prefix').$this->tableName;
+    }
+
+    public function safeTableName() {
+        return $this->db->escapeName($this->tableName());
+    }
+
     public function findById(int $id) {
-        $sql = 'select * from '.$this->db->escapeName($this->table).' where id = :id limit 1';
+        $sql = "select * from {$this->safeTableName()} where id = :id limit 1";
         return $this->db->fetch($sql, [':id' => $id]);
     }
 
@@ -40,19 +49,19 @@ abstract class Repository {
     }
 
     public function deleteById(int $id) {
-        $sql = "delete from ".$this->db->escapeName($this->table)." where id = :id limit 1";
+        $sql = "delete from {$this->safeTableName()} where id = :id limit 1";
         $this->db->query($sql, [':id' => $id]);
     }
 
     public function deleteByIds(array $ids) {
         list($condition, $params) = $this->db->getInConditionAndParams($ids);
-        $sql = "delete from ".$this->db->escapeName($this->table)." where id in ($condition)";
+        $sql = "delete from {$this->safeTableName()} where id in ($condition)";
         $this->db->query($sql, $params);
     }
 
     protected function getSelect($fields, array $params) {
         if ($fields == null) {
-            $fields = $this->allFields;
+            $fields = $this->allFields();
         }
         $select = [];
         $this->orderByFields = [];
@@ -66,8 +75,7 @@ abstract class Repository {
                 $select[] = $escapedName.' as '.$this->db->escapeName($as);
             }
         }
-        $table = $this->db->escapeName($this->table);
-        $sql = 'select '.join(', ', $select).' from '.$table;
+        $sql = 'select '.join(', ', $select).' from '.$this->safeTableName();
         $sql .= $this->getJoins($fields, $params);
         return $sql;
     }
@@ -105,12 +113,12 @@ abstract class Repository {
     }
 
     public function insert(array $data) {
-        $this->db->insert($this->table, $data);
+        $this->db->insert($this->tableName(), $data);
         return $this->db->lastInsertId();
     }
 
     public function update(array $data, string $condition='', array $conditionParams=[]) {
-        $this->db->update($this->table, $data, $condition, $conditionParams);
+        $this->db->update($this->tableName(), $data, $condition, $conditionParams);
     }
 
 }
